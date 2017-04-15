18ce9c4d1ed6cb02c7a84fc08e43a2f40b23a8e8
Alexei Samokvalov <alexei@sam701.com>
2017-04-15 15:07:34 +0200

    Create link local address on a different interface than the internal network

--- ansible/roles/consul/templates/link_local.sh
+++ ansible/roles/consul/templates/link_local.sh
@@ -2,5 +2,5 @@
 
 ip addr | grep {{consul_client_ip}} > /dev/null
 if [ $? != 0 ]; then
-  ip address add dev {{cluster_network_interface}} scope link {{consul_client_ip}}/16
+  ip address add dev {{consul_client_interface}} scope link {{consul_client_ip}}/16
 fi
